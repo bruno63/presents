832cead9c40b5c07851fa599117a8a9e6c8ef844
Bruno Kaiser <bruno@bkaiser.ch>
2015-02-05 17:52:01 +0100

    cleanup for calling external REST service

--- presents.config.js
+++ presents.config.js
@@ -8,19 +8,5 @@ angular.module('presents', ['core'])
 		templateUrl: 'app/presents/list.html',
 		controller: 'PresentsListCtrl',
 		authenticate: true
-	})
-	// sub-state that opens the form inline above of the table
-	.state('presents.addInline', {
-		url: '/add',
-		templateUrl: 'app/presents/addInline.html',
-		controller: 'PresentsAddCtrl',
-		authenticate: true
-	})
-	// state that opens the form in a separate view
-	.state('presentsAdd', {
-		url: '/addPresents',
-		templateUrl: 'app/presents/add.html',
-		controller: 'PresentsAddCtrl',
-		authenticate: true
 	});
 });
